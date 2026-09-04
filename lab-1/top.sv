51c7554893b519899b71a8a513c45ee12fd18d6b
Connor Jones <conjones@g.hmc.edu>
2026-09-03 22:58:27 -0700

    get the leds and switches working

--- lab-1/top.sv
+++ lab-1/top.sv
@@ -1,23 +1,19 @@
 module top(
-	input 	logic	mcu_blink_in,
-	output 	logic fpga_blink_out, mcu_echo_led
+	input logic [3:0] s,
+	output logic [2:0] led
 );
-
 	logic int_osc;
-	logic pulse;
-	logic led_state = 0;
 	logic [24:0] counter = 0;
 	
 	// Internal high-speed oscillator
 	SB_HFOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
 	
 	// Simple clock divider
-	always_ff @(posedge int_osc)
-		begin
-			counter <= counter + 1;
-		end
-		
-  assign fpga_blink_out = counter[24];
-	assign mcu_echo_led = mcu_blink_in;
+	always_ff @(posedge int_osc) begin
+		counter <= counter + 1;
+	end
 
+	assign led[0] = s[1] ^ s[0];
+	assign led[1] = s[3] & s[2];
+	assign led[2] = counter[24];
 endmodule
