5b0ce542038b7957006ca2d020b6de36337844cc
Connor Jones <conjones@g.hmc.edu>
2026-09-04 17:28:25 -0700

    got 7 segment display working

---
+++ lab-1/lab1_cj.sv
@@ -0,0 +1,26 @@
+module lab1_cj(
+	input logic [3:0] switches,
+	output logic [2:0] leds,
+	output logic [6:0] seven_segment_leds
+);
+	// Internal high-speed oscillator
+	logic hf_osc_clk;
+	SB_HFOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(hf_osc_clk));
+
+	// Clock counter to divide frequency
+	logic led_clk;
+	clk_freq_divider #(.WIDTH(32), .MAXCOUNT(10000000)) ctr (
+		.clk_in(hf_osc_clk),
+		.enable(1'b1),
+		.reset(1'b0),
+		.clk_out(led_clk)
+	);
+
+	// Combinational logic for switches
+	assign leds[0] = switches[1] ^ switches[0];
+	assign leds[1] = switches[3] & switches[2];
+	assign leds[2] = led_clk;
+
+	// Combinational logic for 7-segment display
+	seven_segment_encoder encoder (.digit(switches), .segments(seven_segment_leds));
+endmodule
