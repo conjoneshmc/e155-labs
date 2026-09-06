11e388bcda4328bb2ecd0615f189e18b0d37fc89
Connor Jones <conjones@g.hmc.edu>
2026-09-06 16:36:57 -0700

    added comment about timing violation

--- lab-1/lab1_cj.sv
+++ lab-1/lab1_cj.sv
@@ -1,10 +1,12 @@
 module lab1_cj(
 	input logic reset_bar, // Push buttons on dev board are active LOW
-	input logic [3:0] switches,
+	input logic [3:0] switches_bar,
 	output logic [2:0] leds,
 	output logic [6:0] seven_segment_leds
 );
 	// Internal high-speed oscillator
+	// Have to run the clock at a lower frequency because I was getting timing violations
+	// The clock runs at 24MHz, I adjusted MAXCOUNT to compensate
 	logic hf_osc_clk;
 	SB_HFOSC #(.CLKHF_DIV("0b01")) hf_osc (
 		.CLKHFPU(1'b1),
@@ -17,18 +19,18 @@ module lab1_cj(
 	clk_freq_divider #(.WIDTH(32), .MAXCOUNT(5000000)) ctr (
 		.clk_in(hf_osc_clk),
 		.enable(1'b1),
-		.reset(!reset_bar),
+		.reset(~reset_bar),
 		.clk_out(led_clk)
 	);
 
 	// Combinational logic for switches
 	// NOTE: The switch pins are pulled HIGH when the switches are off, so we have
 	// to invert them in our code
-	assign leds[0] = (~switches[1]) ^ (~switches[0]);
-	assign leds[1] = (~switches[3]) & (~switches[2]);
+	assign leds[0] = (~switches_bar[1]) ^ (~switches_bar[0]);
+	assign leds[1] = (~switches_bar[3]) & (~switches_bar[2]);
 	assign leds[2] = led_clk;
 
 	// Combinational logic for 7-segment display
 	// Same note as above, switch pins are active low
-	seven_segment_encoder encoder (.digit(~switches), .segments(seven_segment_leds));
+	seven_segment_encoder encoder (.digit(~switches_bar), .segments(seven_segment_leds));
 endmodule
