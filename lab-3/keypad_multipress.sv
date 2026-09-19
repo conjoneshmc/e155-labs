333d9e552383202bf217e3bb4d5c48d969676b4d
Connor Jones <conjones@g.hmc.edu>
2026-09-18 20:12:56 -0700

    removed extra state from keypad_multipress module

--- lab-3/keypad_multipress.sv
+++ lab-3/keypad_multipress.sv
@@ -4,10 +4,9 @@ module keypad_multipress(
   input logic [3:0] num_pressed,
   output logic should_update
 );
-  // 0 = nothing being pressed
+  // 0 = nothing or multiple buttons being pressed
   // 1 = exactly one button being pressed
-  // 2 = multipress condition
-  logic [1:0] state;
+  logic state;
 
   // Next state logic
   always_ff @(posedge clk) begin
@@ -15,14 +14,13 @@ module keypad_multipress(
       state <= 0;
     end
     else begin
-      if      (num_pressed == 0) state <= 0;
-      else if (num_pressed == 1) state <= 1;
-      else                       state <= 2;
+      if (num_pressed == 1) state <= 1;
+      else                  state <= 0;
     end
   end
 
   // Output logic (mealy state machine)
   // We should only update the display if we're entering a state of a single button press
   // from a multi-press condition or a nothing-pressed condition
-  assign should_update = (state == 0 | state == 2) & (num_pressed == 1);
+  assign should_update = (state == 0) & (num_pressed == 1);
 endmodule
